070e2718ac2b0cc5caa2543f7e752350a21db2c2
oustrix <50222966+oustrix@users.noreply.github.com>
2023-05-10 01:03:17 +0300

    create Service usecase

---
+++ internal/usecase/service.go
@@ -0,0 +1,29 @@
+package usecase
+
+import (
+	"vk_task2/internal/domain"
+	"vk_task2/internal/repository/postgres/service"
+)
+
+type serviceUsecase struct {
+	repository service.Repository
+}
+
+type Service interface {
+	List(*domain.User) ([]*domain.Service, error)
+	Create(*domain.Service) error
+}
+
+func NewServiceUsecase(repository service.Repository) Service {
+	return &serviceUsecase{
+		repository: repository,
+	}
+}
+
+func (u *serviceUsecase) List(user *domain.User) ([]*domain.Service, error) {
+	return u.repository.GetByUserID(user.ID)
+}
+
+func (u *serviceUsecase) Create(service *domain.Service) error {
+	return u.repository.Create(service)
+}
