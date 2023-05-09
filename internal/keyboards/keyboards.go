8845f196690a89ecbb4f6824627cee1cad93cac3
oustrix <50222966+oustrix@users.noreply.github.com>
2023-05-10 02:50:38 +0300

    change Создать to Добавить

--- internal/keyboards/keyboards.go
+++ internal/keyboards/keyboards.go
@@ -5,7 +5,7 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 func Basic() tgbotapi.ReplyKeyboardMarkup {
 	return tgbotapi.NewReplyKeyboard(
 		tgbotapi.NewKeyboardButtonRow(
-			tgbotapi.NewKeyboardButton("Создать"),
+			tgbotapi.NewKeyboardButton("Добавить"),
 			tgbotapi.NewKeyboardButton("Получить"),
 			tgbotapi.NewKeyboardButton("Удалить"),
 		),
