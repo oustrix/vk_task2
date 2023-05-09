9e05cd91971b6ea930d082ca3eb3ec0e2f91971a
oustrix <50222966+oustrix@users.noreply.github.com>
2023-05-10 00:52:50 +0300

    add Service to migrate function

--- internal/app/migrate.go
+++ internal/app/migrate.go
@@ -1,5 +1,8 @@
 package app
 
+import "vk_task2/internal/repository/postgres/service"
+
 func (a *App) migrate() error {
-	return a.db.AutoMigrate()
+	return a.db.AutoMigrate(
+		&service.Service{})
 }
